30f7442536eabdc0283346b3795904531e5e7fdd
Ahmed Esmail <97197954+AhmedEsmail8@users.noreply.github.com>
2023-11-16 13:24:25 +0200

    Task #2 init and create GetData class

---
+++ Task #2/NewspaperSellerSimulation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewspaperSellerTesting;
+using NewspaperSellerModels;
+
+namespace NewspaperSellerSimulation
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            SimulationSystem system = new SimulationSystem();
+            GetData data = new GetData(Constants.FileNames.TestCase1);
+            Console.WriteLine(data.ToString());
+            data.FillSystem(ref system);
+
+            string result = TestingManager.Test(system, Constants.FileNames.TestCase1);
+            MessageBox.Show(result);
+
+            Form1 form = new Form1();
+            Application.Run(form);
+        }
+    }
+}
