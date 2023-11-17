4667ee947827b3a2a3713c91b6da2fa2af3576bd
EbrahimTarek02 <ebrahimtarek2002@gmail.com>
2023-11-17 14:15:48 +0200

    Finished Simulation Table

--- Task #2/NewspaperSellerModels/SimulationSystem.cs
+++ Task #2/NewspaperSellerModels/SimulationSystem.cs
@@ -8,6 +8,8 @@ namespace NewspaperSellerModels
 {
     public class SimulationSystem
     {
+        public Random random = new Random();
+
         public SimulationSystem()
         {
             DayTypeDistributions = new List<DayTypeDistribution>();
@@ -15,6 +17,7 @@ namespace NewspaperSellerModels
             SimulationTable = new List<SimulationCase>();
             PerformanceMeasures = new PerformanceMeasures();
         }
+
         ///////////// INPUTS /////////////
         public int NumOfNewspapers { get; set; }
         public int NumOfRecords { get; set; }
@@ -28,5 +31,119 @@ namespace NewspaperSellerModels
         ///////////// OUTPUTS /////////////
         public List<SimulationCase> SimulationTable { get; set; }
         public PerformanceMeasures PerformanceMeasures { get; set; }
+
+        public void start()
+        {
+            for (int i = 0; i < NumOfRecords; i++)
+            {
+                SimulationCase simulationCase = new SimulationCase();
+
+                // day Column
+                simulationCase.DayNo = i + 1;
+
+                // Rondom Digits for type of newsday
+                int randomDayTypeNumber = random.Next(1, 101);
+                simulationCase.RandomNewsDayType = randomDayTypeNumber;
+
+                // Type of Newsday
+                simulationCase.NewsDayType = generateRandomDay(randomDayTypeNumber);
+
+                // Rondom Digits for demand
+                int randomDemandNumber = random.Next(1, 101);
+                simulationCase.RandomDemand = randomDemandNumber;
+
+                // Demand
+                simulationCase.Demand = generateRandomDemand(randomDemandNumber, simulationCase.NewsDayType);
+
+                // Daily Cost
+                simulationCase.DailyCost = NumOfNewspapers * PurchasePrice;
+
+                // Revenue from sales
+                simulationCase.SalesProfit = calculateRevenuFromSales(simulationCase.Demand);
+
+                // Lost profit from excess demand
+                if (simulationCase.Demand > NumOfNewspapers)
+                {
+                    simulationCase.LostProfit = calculateLostProfit(simulationCase.Demand);
+                }
+                else
+                {
+                    simulationCase.LostProfit = 0;
+                }
+
+                // Salvage From sale of scrape
+                if (NumOfNewspapers > simulationCase.Demand)
+                {
+                    simulationCase.ScrapProfit = calculateSalvage(simulationCase.Demand);
+                }
+                else
+                {
+                    simulationCase.ScrapProfit = 0;
+                }
+
+                // Daily profit
+                simulationCase.DailyNetProfit = calculateDailyCost(simulationCase.SalesProfit, simulationCase.DailyCost, simulationCase.LostProfit, simulationCase.ScrapProfit);
+
+                SimulationTable.Add(simulationCase);
+            }
+        }
+
+        public Enums.DayType generateRandomDay(int randomDayTypeNumber)
+        {
+            foreach (DayTypeDistribution day in DayTypeDistributions)
+            {
+                if (randomDayTypeNumber >= day.MinRange && randomDayTypeNumber <= day.MaxRange)
+                {
+                    return day.DayType;
+                }
+            }
+
+            return 0;
+        }
+
+        public int generateRandomDemand(int randomDemandNumber, Enums.DayType dayType)
+        {
+            foreach (DemandDistribution demand in DemandDistributions)
+            {
+                foreach (DayTypeDistribution day in demand.DayTypeDistributions)
+                {
+                    if (dayType == day.DayType && randomDemandNumber >= day.MinRange && randomDemandNumber <= day.MaxRange)
+                    {
+                        return demand.Demand;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public decimal calculateRevenuFromSales(int demand)
+        {
+            decimal cost = 0;
+            if (demand <= NumOfNewspapers)
+            {
+                cost = demand * SellingPrice;
+            }
+            else
+            {
+                cost = NumOfNewspapers * SellingPrice;
+            }
+
+            return cost;
+        }
+
+        public decimal calculateLostProfit(int demand)
+        {
+            return (demand - NumOfNewspapers) * (SellingPrice - PurchasePrice);
+        }
+
+        public decimal calculateSalvage(int demand)
+        {
+            return (NumOfNewspapers - demand) * ScrapPrice;
+        }
+
+        public decimal calculateDailyCost(decimal salesProfit, decimal costOfNewsPapers, decimal lostProfit, decimal salvage)
+        {
+            return salesProfit - costOfNewsPapers - lostProfit + salvage;
+        }
     }
 }
--- Task #2/NewspaperSellerSimulation/Program.cs
+++ Task #2/NewspaperSellerSimulation/Program.cs
@@ -20,15 +20,37 @@ namespace NewspaperSellerSimulation
             Application.SetCompatibleTextRenderingDefault(false);
 
             SimulationSystem system = new SimulationSystem();
+
             GetData data = new GetData(Constants.FileNames.TestCase1);
             Console.WriteLine(data.ToString());
             data.FillSystem(ref system);
 
+            system.start();
+            PrintSimulationTable(system.SimulationTable);
+
             string result = TestingManager.Test(system, Constants.FileNames.TestCase1);
+            
+            
             MessageBox.Show(result);
 
             Form1 form = new Form1();
             Application.Run(form);
+        }
+
+        static void PrintSimulationTable(List<SimulationCase> simulationTable)
+        {
+            Console.WriteLine("SimulationTable:");
+            Console.WriteLine("Day | Rondom Digits for Type of Newsday | Type of Newsday | Rondom Digits for Demand | Demand | Revenue from Sales | Lost Profit from Excess Demand | Salvage from Sale of Scrap | Daily Profit");
+
+            foreach (var item in simulationTable)
+            {
+                string formattedRow = string.Format("{0, -3} | {1, -33} | {2, -15} | {3, -24} | {4, -6} | {5, -18} | {6, -30} | {7, -26} | {8, -7}",
+                    item.DayNo, item.RandomNewsDayType, item.NewsDayType, item.RandomDemand,
+                    item.Demand, item.DailyCost, item.LostProfit, item.ScrapProfit, item.DailyNetProfit
+                );
+
+                Console.WriteLine(formattedRow);
+            }
         }
     }
 }
